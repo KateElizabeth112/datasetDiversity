a0221905e6ee9a99544f6b1b6e9a2acbb0638824
Kate Cevora <kate.wilkinson@illumr.com>
2024-04-09 10:30:12 +0100

    Changed summation strategy

--- resampleAndCrop.py
+++ resampleAndCrop.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import numpy as np
 import pickle as pkl
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Just an example",  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d", "--dataset", default="TS", help="Task to evaluate")
@@ -106,9 +107,9 @@ def getExtents(organ):
             # Check that this sums to more than zero
             if np.sum(label_idx) > 0:
                 # sum along each pair of axes to find the maximum extent for the third axis
-                x_sum = np.sum(label_idx, axis=(1, 2))
-                y_sum = np.sum(label_idx, axis=(0, 2))
-                z_sum = np.sum(label_idx, axis=(0, 1))
+                x_sum = np.sum(label_idx, axis=1)
+                y_sum = np.sum(label_idx, axis=2)
+                z_sum = np.sum(label_idx, axis=0)
 
                 x_extents.append(np.max(x_sum))
                 y_extents.append(np.max(y_sum))
@@ -128,14 +129,73 @@ def getExtents(organ):
                                                                         np.max(np.array(z_extents))))
 
 
-def crop(organ_idx, crop_extent):
+def crop(organ, crop_extent):
     # Crop the images to a window of size crop_extend, centering the organ in the volume
     print("Crop images")
 
+    filenames = os.listdir(label_dir)
+
+    organ_idx = organ_dict.get(organ)
+
+    for fn in filenames:
+        if fn.endswith(".nii.gz"):
+            # Load the original label
+            label_nii = nib.load(os.path.join(label_dir, fn))
+
+            # convert label to numpy array
+            label = label_nii.get_fdata()
+
+            # Filter out the label idx only
+            label_idx = np.zeros(label.shape)
+            label_idx[label == organ_idx] = 1
+
+            # Check that this sums to more than zero
+            if np.sum(label_idx) > 0:
+                # sum along each axis to find the maximum extent for the third axis
+                x_sum = np.sum(label_idx, axis=(1, 2))
+                y_sum = np.sum(label_idx, axis=(0, 2))
+                z_sum = np.sum(label_idx, axis=(0, 1))
+
+                # find the index of the start and end point, and the origin as the midpoint
+                x_1 = np.nonzero(x_sum)[0][0]
+                x_2 = np.nonzero(x_sum)[0][-1]
+                x_0 = int(np.round(0.5 * (x_1 + x_2)))
+
+                y_1 = np.nonzero(y_sum)[0][0]
+                y_2 = np.nonzero(y_sum)[0][-1]
+                y_0 = int(np.round(0.5 * (y_1 + y_2)))
+
+                z_1 = np.nonzero(z_sum)[0][0]
+                z_2 = np.nonzero(z_sum)[0][-1]
+                z_0 = int(np.round(0.5 * (z_1 + z_2)))
+
+                # get the bounding box for the x_z_plane
+                x_min = int(x_0 - crop_extent[0] / 2)
+                x_max = int(x_0 + crop_extent[0] / 2)
+
+                z_min = int(z_0 - crop_extent[2] / 2)
+                z_max = int(z_0 + crop_extent[2] / 2)
+
+                # plot the x_z plane sliced through the y-origin
+                x_z_plane = label_idx[:, y_0, :]
+                plt.clf()
+                plt.imshow(np.rot90(x_z_plane), origin='lower')
+                #plt.axvline(z_min)
+                #plt.axvline(z_max)
+                #plt.axhline(x_min)
+                #plt.axhline(x_max)
+                #plt.scatter(z_0, x_0)
+                plt.axvline(x_0)
+                #plt.axvline(x_1)
+                #plt.axvline(x_2)
+                plt.show()
+
+
 
 def main():
-    resample()
+    #resample()
     getExtents("liver")
+    #crop("liver", (60, 60, 60))
 
 if __name__ == "__main__":
     main()
