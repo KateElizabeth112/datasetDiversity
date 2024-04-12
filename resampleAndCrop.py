0b5bfb9abcc7a7107535b9ee9c6700f3eeb6c575
Kate Cevora <kate.wilkinson@illumr.com>
2024-04-12 11:33:38 +0100

    added try excpet clause to catch problematic files

--- resampleAndCrop.py
+++ resampleAndCrop.py
@@ -158,108 +158,111 @@ def crop(organ):
     for fn in filenames:
         if fn.endswith(".nii.gz"):
             # Load the original label
-            label_nii = nib.load(os.path.join(label_dir, fn))
-            label = label_nii.get_fdata()   # convert label to numpy array
+            try:
+                label_nii = nib.load(os.path.join(label_dir, fn))
+                label = label_nii.get_fdata()   # convert label to numpy array
 
-            # load the corresponding image
-            image_nii = nib.load(os.path.join(image_dir, fn[:9] + "_0000.nii.gz"))
-            image = image_nii.get_fdata()  # convert image to numpy array
+                # load the corresponding image
+                image_nii = nib.load(os.path.join(image_dir, fn[:9] + "_0000.nii.gz"))
+                image = image_nii.get_fdata()  # convert image to numpy array
+            except:
+                print("File {} could not be opened".format(fn))
+            else:
+                # Filter out the label idx only
+                label_idx = np.zeros(label.shape)
+                label_idx[label == organ_idx] = 1
 
-            # Filter out the label idx only
-            label_idx = np.zeros(label.shape)
-            label_idx[label == organ_idx] = 1
+                # Check that this sums to more than zero
+                if np.sum(label_idx) > 0:
+                    # sum along each pair of axes to find the plane with maximum area for the third axis
+                    x_area = np.sum(label_idx, axis=(1, 2))
+                    y_area = np.sum(label_idx, axis=(0, 2))
+                    z_area = np.sum(label_idx, axis=(0, 1))
 
-            # Check that this sums to more than zero
-            if np.sum(label_idx) > 0:
-                # sum along each pair of axes to find the plane with maximum area for the third axis
-                x_area = np.sum(label_idx, axis=(1, 2))
-                y_area = np.sum(label_idx, axis=(0, 2))
-                z_area = np.sum(label_idx, axis=(0, 1))
+                    # find the index of the start and end point, and the origin as the midpoint
+                    x_1 = np.nonzero(x_area)[0][0]
+                    x_2 = np.nonzero(x_area)[0][-1]
+                    x_0 = int(np.round(0.5 * (x_1 + x_2)))
 
-                # find the index of the start and end point, and the origin as the midpoint
-                x_1 = np.nonzero(x_area)[0][0]
-                x_2 = np.nonzero(x_area)[0][-1]
-                x_0 = int(np.round(0.5 * (x_1 + x_2)))
+                    y_1 = np.nonzero(y_area)[0][0]
+                    y_2 = np.nonzero(y_area)[0][-1]
+                    y_0 = int(np.round(0.5 * (y_1 + y_2)))
 
-                y_1 = np.nonzero(y_area)[0][0]
-                y_2 = np.nonzero(y_area)[0][-1]
-                y_0 = int(np.round(0.5 * (y_1 + y_2)))
+                    z_1 = np.nonzero(z_area)[0][0]
+                    z_2 = np.nonzero(z_area)[0][-1]
+                    z_0 = int(np.round(0.5 * (z_1 + z_2)))
 
-                z_1 = np.nonzero(z_area)[0][0]
-                z_2 = np.nonzero(z_area)[0][-1]
-                z_0 = int(np.round(0.5 * (z_1 + z_2)))
+                    # get the bounding box for all planes
+                    x_min = int(x_0 - crop_extent[0] / 2)
+                    x_max = int(x_0 + crop_extent[0] / 2)
 
-                # get the bounding box for all planes
-                x_min = int(x_0 - crop_extent[0] / 2)
-                x_max = int(x_0 + crop_extent[0] / 2)
+                    y_min = int(y_0 - crop_extent[1] / 2)
+                    y_max = int(y_0 + crop_extent[1] / 2)
 
-                y_min = int(y_0 - crop_extent[1] / 2)
-                y_max = int(y_0 + crop_extent[1] / 2)
+                    z_min = int(z_0 - crop_extent[2] / 2)
+                    z_max = int(z_0 + crop_extent[2] / 2)
 
-                z_min = int(z_0 - crop_extent[2] / 2)
-                z_max = int(z_0 + crop_extent[2] / 2)
+                    # crop the label and image
+                    label_crop = label_idx[np.max((0, x_min)):x_max, np.max((0, y_min)):y_max, np.max((0, z_min)):z_max]
+                    image_crop = image[np.max((0, x_min)):x_max, np.max((0, y_min)):y_max, np.max((0, z_min)):z_max]
 
-                # crop the label and image
-                label_crop = label_idx[np.max((0, x_min)):x_max, np.max((0, y_min)):y_max, np.max((0, z_min)):z_max]
-                image_crop = image[np.max((0, x_min)):x_max, np.max((0, y_min)):y_max, np.max((0, z_min)):z_max]
+                    # check if we need to do any padding (i.e. if the cropping planes are outside bounds of original image)
+                    if np.sum(crop_extent) > np.sum(label_crop.shape):
 
-                # check if we need to do any padding (i.e. if the cropping planes are outside bounds of original image)
-                if np.sum(crop_extent) > np.sum(label_crop.shape):
+                        if x_min >= 0:
+                            x_min_offset = 0
+                        elif x_min < 0:
+                            x_min_offset = abs(x_min)
 
-                    if x_min >= 0:
-                        x_min_offset = 0
-                    elif x_min < 0:
-                        x_min_offset = abs(x_min)
+                        if y_min >= 0:
+                            y_min_offset = 0
+                        elif y_min < 0:
+                            y_min_offset = abs(y_min)
 
-                    if y_min >= 0:
-                        y_min_offset = 0
-                    elif y_min < 0:
-                        y_min_offset = abs(y_min)
+                        if z_min >= 0:
+                            z_min_offset = 0
+                        elif z_min < 0:
+                            z_min_offset = abs(z_min)
 
-                    if z_min >= 0:
-                        z_min_offset = 0
-                    elif z_min < 0:
-                        z_min_offset = abs(z_min)
+                        label_crop_padded = np.zeros(crop_extent)
+                        image_crop_padded = np.zeros(crop_extent)
 
-                    label_crop_padded = np.zeros(crop_extent)
-                    image_crop_padded = np.zeros(crop_extent)
+                        label_crop_padded.fill(0.5)
+                        image_crop_padded.fill(np.mean(image_crop))
 
-                    label_crop_padded.fill(0.5)
-                    image_crop_padded.fill(np.mean(image_crop))
+                        label_crop_padded[x_min_offset:x_min_offset + label_crop.shape[0],
+                        y_min_offset:y_min_offset + label_crop.shape[1],
+                        z_min_offset:z_min_offset + label_crop.shape[2]] = label_crop
 
-                    label_crop_padded[x_min_offset:x_min_offset + label_crop.shape[0],
-                    y_min_offset:y_min_offset + label_crop.shape[1],
-                    z_min_offset:z_min_offset + label_crop.shape[2]] = label_crop
+                        image_crop_padded[x_min_offset:x_min_offset + label_crop.shape[0],
+                        y_min_offset:y_min_offset + label_crop.shape[1],
+                        z_min_offset:z_min_offset + label_crop.shape[2]] = image_crop
 
-                    image_crop_padded[x_min_offset:x_min_offset + label_crop.shape[0],
-                    y_min_offset:y_min_offset + label_crop.shape[1],
-                    z_min_offset:z_min_offset + label_crop.shape[2]] = image_crop
+                        label_crop = label_crop_padded
+                        image_crop = image_crop_padded
 
-                    label_crop = label_crop_padded
-                    image_crop = image_crop_padded
+                    # plot each plane sliced through the centre of the image
+                    plt.clf()
+                    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(16, 6))
+                    ax1, ax2, ax3, ax4, ax5, ax6 = axes.flatten()
 
-                # plot each plane sliced through the centre of the image
-                plt.clf()
-                fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(16, 6))
-                ax1, ax2, ax3, ax4, ax5, ax6 = axes.flatten()
+                    ax1.imshow(np.rot90(label_crop[int(crop_extent[0]/2), :, :]), origin='lower')
+                    ax2.imshow(np.rot90(label_crop[:, int(crop_extent[1]/2), :]), origin='lower')
+                    ax3.imshow(np.rot90(label_crop[:, :, int(crop_extent[2]/2)]), origin='lower')
 
-                ax1.imshow(np.rot90(label_crop[int(crop_extent[0]/2), :, :]), origin='lower')
-                ax2.imshow(np.rot90(label_crop[:, int(crop_extent[1]/2), :]), origin='lower')
-                ax3.imshow(np.rot90(label_crop[:, :, int(crop_extent[2]/2)]), origin='lower')
+                    ax4.imshow(np.rot90(image_crop[int(crop_extent[0]/2), :, :]), origin='lower', cmap="gray")
+                    ax5.imshow(np.rot90(image_crop[:, int(crop_extent[1]/2), :]), origin='lower', cmap="gray")
+                    ax6.imshow(np.rot90(image_crop[:, :, int(crop_extent[2]/2)]), origin='lower', cmap="gray")
 
-                ax4.imshow(np.rot90(image_crop[int(crop_extent[0]/2), :, :]), origin='lower', cmap="gray")
-                ax5.imshow(np.rot90(image_crop[:, int(crop_extent[1]/2), :]), origin='lower', cmap="gray")
-                ax6.imshow(np.rot90(image_crop[:, :, int(crop_extent[2]/2)]), origin='lower', cmap="gray")
+                    # save the image
+                    # check we have a directory to save the images
+                    if not os.path.exists(os.path.join(root_dir, "images", "crops")):
+                        os.mkdir(os.path.join(root_dir, "images", "crops"))
 
-                # save the image
-                # check we have a directory to save the images
-                if not os.path.exists(os.path.join(root_dir, "images", "crops")):
-                    os.mkdir(os.path.join(root_dir, "images", "crops"))
+                    if not os.path.exists(os.path.join(root_dir, "images", "crops", organ)):
+                        os.mkdir(os.path.join(root_dir, "images", "crops", organ))
 
-                if not os.path.exists(os.path.join(root_dir, "images", "crops", organ)):
-                    os.mkdir(os.path.join(root_dir, "images", "crops", organ))
-
-                plt.savefig(os.path.join(root_dir, "images", "crops", organ, fn[:9] + ".png"))
+                    plt.savefig(os.path.join(root_dir, "images", "crops", organ, fn[:9] + ".png"))
 
 
 def main():
